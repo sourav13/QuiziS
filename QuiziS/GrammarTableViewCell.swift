9cb20f3a887829e950fa8200b1da092db75760a5
Sourav Sachdeva <souravsachdeva13@gmail.com>
2020-01-14 14:41:25 -0800

    added initial views

---
+++ QuiziS/GrammarTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  GrammarTableViewCell.swift
+//  QuiziS
+//
+//  Created by sourav sachdeva on 14/01/20.
+//  Copyright © 2020 sourav sachdeva. All rights reserved.
+//
+
+import UIKit
+
+class GrammarTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var grammarTypeButton: UIButton!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ QuiziS/GrammarTableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  GrammarTableViewController.swift
+//  QuiziS
+//
+//  Created by sourav sachdeva on 14/01/20.
+//  Copyright © 2020 sourav sachdeva. All rights reserved.
+//
+
+import UIKit
+
+class GrammarTableViewController: UITableViewController {
+
+    var grammarTypeLabels = ["Prepositions","Irregular verbs","Adjectives & adverbs","Countable and uncountable","Infinitives or gerunds","More or less","Expressions with colour","Few and little","Adverbial Clauses","Modals","Linkers","Conjunctions"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return grammarTypeLabels.count
+    }
+
+ 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! GrammarTableViewCell
+        
+        cell.grammarTypeButton.setTitle(grammarTypeLabels[indexPath.row], for: .normal)
+
+        return cell
+    }
+ 
+
+  
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+
+}
