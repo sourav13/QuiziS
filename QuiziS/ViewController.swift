b46e07e09971114db95843de18f22a0857ba1a10
Sourav Sachdeva <souravsachdeva13@gmail.com>
2020-01-14 12:40:11 -0800

    Initial Commit

---
+++ QuiziS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuiziS
+//
+//  Created by sourav sachdeva on 14/01/20.
+//  Copyright © 2020 sourav sachdeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
