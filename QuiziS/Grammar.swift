c273462cf90d02970981aecd4d1b21aa3f3da1d2
Sourav Sachdeva <souravsachdeva13@gmail.com>
2020-01-15 13:14:49 -0800

    changed to read questions from plist

--- QuiziS/Grammar.swift
+++ QuiziS/Grammar.swift
@@ -8,116 +8,26 @@
 
 import Foundation
 class Grammar{
-    static  var questions = [Question]()
-    static func getCurrentQuestion(currentQuestion:Int)->Question{
-         return questions[currentQuestion]
-     }
-static func prepositions(){
-       questions.removeAll()
-       let question0 = Question(questionText: "I'll be at  home ____ Friday morning.", options: ["at","on","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question1 = Question(questionText: "Rohan is looking ____ you.", options: ["at","for","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question2 = Question(questionText: "We arrived at the airport ____ 2 hours before our flight", options: ["about","for","in","behind"], correctAns: 0, wrongAns: -1, isAnswered: false)
-       questions.append(question0)
-       questions.append(question1)
-       questions.append(question2)
-   }
-  static func irregularVerbs(){
-       questions.removeAll()
-       let question0 = Question(questionText: "He likes to ____ motorcycles.", options: ["x","x","x","xx"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question1 = Question(questionText: "Im so sorry, I ____ to bring it.", options: ["at","for","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question2 = Question(questionText: "We arrived at the airport ____ 2 hours before our flight", options: ["x","x","x","xx"], correctAns: 0, wrongAns: -1, isAnswered: false)
-       questions.append(question0)
-       questions.append(question1)
-       questions.append(question2)
-   }
- static  func adjectivesAndAdVerbs(){
-       questions.removeAll()
-       let question0 = Question(questionText: "I like ____ fruit.", options: ["at","on","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question1 = Question(questionText: "We had ____ pancakes for breakfast.", options: ["at","for","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question2 = Question(questionText: "We arrived at the airport ____ 2 hours before our flight", options: ["about","for","in","behind"], correctAns: 0, wrongAns: -1, isAnswered: false)
-       questions.append(question0)
-       questions.append(question1)
-       questions.append(question2)
-   }
- static  func countableAndUncountables(){
-        questions.removeAll()
-       let question0 = Question(questionText: "I'll be at  home ____ Friday morning Friday morning Friday morning Friday morning Friday morning Friday morning Friday morning Friday morning Friday morning.", options: ["at","on","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question1 = Question(questionText: "Rohan is looking ____ you.", options: ["at","for","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question2 = Question(questionText: "We arrived at the airport ____ 2 hours before our flight", options: ["about","for","in","behind"], correctAns: 0, wrongAns: -1, isAnswered: false)
-       questions.append(question0)
-       questions.append(question1)
-       questions.append(question2)
-   }
- static  func infinitivesOrGerunds(){
-    questions.removeAll()
-       let question0 = Question(questionText: "I'll be at  home ____ Friday morning.", options: ["at","on","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question1 = Question(questionText: "Rohan is looking ____ you.", options: ["at","for","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question2 = Question(questionText: "We arrived at the airport ____ 2 hours before our flight", options: ["about","for","in","behind"], correctAns: 0, wrongAns: -1, isAnswered: false)
-       questions.append(question0)
-       questions.append(question1)
-       questions.append(question2)
-   }
-  static func MoreOrLess(){
-    questions.removeAll()
-       let question0 = Question(questionText: "I'll be at  home ____ Friday morning.", options: ["at","on","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question1 = Question(questionText: "Rohan is looking ____ you.", options: ["at","for","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question2 = Question(questionText: "We arrived at the airport ____ 2 hours before our flight", options: ["about","for","in","behind"], correctAns: 0, wrongAns: -1, isAnswered: false)
-       questions.append(question0)
-       questions.append(question1)
-       questions.append(question2)
-   }
-  static func expressionsOfColor(){
-    questions.removeAll()
-       let question0 = Question(questionText: "I'll be at  home ____ Friday morning.", options: ["at","on","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question1 = Question(questionText: "Rohan is looking ____ you.", options: ["at","for","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question2 = Question(questionText: "We arrived at the airport ____ 2 hours before our flight", options: ["about","for","in","behind"], correctAns: 0, wrongAns: -1, isAnswered: false)
-       questions.append(question0)
-       questions.append(question1)
-       questions.append(question2)
-   }
-  static func fewAndLittle(){
-    questions.removeAll()
-       let question0 = Question(questionText: "I'll be at  home ____ Friday morning.", options: ["at","on","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question1 = Question(questionText: "Rohan is looking ____ you.", options: ["at","for","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question2 = Question(questionText: "We arrived at the airport ____ 2 hours before our flight", options: ["about","for","in","behind"], correctAns: 0, wrongAns: -1, isAnswered: false)
-       questions.append(question0)
-       questions.append(question1)
-       questions.append(question2)
-   }
-  static func AdverbialClauses(){
-    questions.removeAll()
-       let question0 = Question(questionText: "I'll be at  home ____ Friday morning.", options: ["at","on","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question1 = Question(questionText: "Rohan is looking ____ you.", options: ["at","for","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question2 = Question(questionText: "We arrived at the airport ____ 2 hours before our flight", options: ["about","for","in","behind"], correctAns: 0, wrongAns: -1, isAnswered: false)
-       questions.append(question0)
-       questions.append(question1)
-       questions.append(question2)
-   }
- static  func linkers(){
-    questions.removeAll()
-       let question0 = Question(questionText: "I'll be at  home ____ Friday morning.", options: ["at","on","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question1 = Question(questionText: "Rohan is looking ____ you.", options: ["at","for","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question2 = Question(questionText: "We arrived at the airport ____ 2 hours before our flight", options: ["about","for","in","behind"], correctAns: 0, wrongAns: -1, isAnswered: false)
-       questions.append(question0)
-       questions.append(question1)
-       questions.append(question2)
-   }
- static  func Conjuctions(){
-    questions.removeAll()
-       let question0 = Question(questionText: "I'll be at  home ____ Friday morning.", options: ["at","on","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question1 = Question(questionText: "Rohan is looking ____ you.", options: ["at","for","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question2 = Question(questionText: "We arrived at the airport ____ 2 hours before our flight", options: ["about","for","in","behind"], correctAns: 0, wrongAns: -1, isAnswered: false)
-       questions.append(question0)
-       questions.append(question1)
-       questions.append(question2)
-   }
- static  func Modals(){
-    questions.removeAll()
-       let question0 = Question(questionText: "I'll be at  home ____ Friday morning.", options: ["at","on","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question1 = Question(questionText: "Rohan is looking ____ you.", options: ["at","for","in","behind"], correctAns: 1, wrongAns: -1, isAnswered: false)
-       let question2 = Question(questionText: "We arrived at the airport ____ 2 hours before our flight", options: ["about","for","in","behind"], correctAns: 0, wrongAns: -1, isAnswered: false)
-       questions.append(question0)
-       questions.append(question1)
-       questions.append(question2)
-   }
+    func getPlist(withName name: String) -> AnyObject?
+    {
+        if  let path = Bundle.main.path(forResource: name, ofType: "plist"),
+            let xml = FileManager.default.contents(atPath: path)
+        {
+            return (try? PropertyListSerialization.propertyList(from: xml, options: .mutableContainersAndLeaves, format: nil) as AnyObject?) 
+        }
+        return nil
+    }
+    
+    func grammar(grammarType:String)->[Question]{
+        guard let array =  getPlist(withName: grammarType) as? [AnyObject] else {return []}
+        var questions = [Question]()
+        for x in array{
+            let element = x as? [String:AnyObject]
+            guard let questionText = element?["QuestionText"] as? String else {return []}
+            guard let options = x["options"] as? [String] else {return []}
+            let question = Question(questionText: questionText, options: options , correctAns: element?["correctAns"] as! Int, wrongAns: -1, isAnswered: false)
+            questions.append(question)
+        }
+        return questions
+    }
 }
--- QuiziS/QuestionViewController.swift
+++ QuiziS/QuestionViewController.swift
@@ -25,9 +25,11 @@ class QuestionViewController: UIViewController {
   
     var currentQuestion: Question?
     var currentQuestionNumber: Int = 0
-    var count = 60
+    var count = 59
     var score = 0
     var selectedGrammarType: Int = 0
+    let grammer  = Grammar()
+    var questions = [Question]()
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -36,21 +38,25 @@ class QuestionViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     func loadRespectiveQuestions(){
+
         switch(selectedGrammarType){
-            case 0: Grammar.prepositions()
-            case 1: Grammar.irregularVerbs()
-            case 2: Grammar.adjectivesAndAdVerbs()
-            case 3: Grammar.countableAndUncountables()
-            case 4: Grammar.infinitivesOrGerunds()
-            case 5: Grammar.MoreOrLess()
-            case 6: Grammar.expressionsOfColor()
-            case 7: Grammar.fewAndLittle()
-            case 8: Grammar.AdverbialClauses()
-            case 9: Grammar.Modals()
-            case 10: Grammar.linkers()
-            case 11: Grammar.Conjuctions()
-            default: Grammar.prepositions()
+        case 0: questions = grammer.grammar(grammarType: "prepositions")
+            case 1: questions = grammer.grammar(grammarType: "irregularverbs")
+            case 2: questions = grammer.grammar(grammarType: "adjectivesandadverbs")
+            case 3: questions = grammer.grammar(grammarType: "countableanduncountables")
+            case 4: questions = grammer.grammar(grammarType: "infinitivesorgerunds")
+            case 5: questions = grammer.grammar(grammarType: "moreorless")
+            case 6: questions = grammer.grammar(grammarType: "expressionsofcolor")
+            case 7: questions = grammer.grammar(grammarType: "fewandlittle")
+            case 8: questions = grammer.grammar(grammarType: "adverbialclauses")
+            case 9: questions = grammer.grammar(grammarType: "modals")
+            case 10: questions = grammer.grammar(grammarType: "linkers")
+            case 11: questions = grammer.grammar(grammarType: "conjunctions")
+        default: questions = grammer.grammar(grammarType: "prepositions")
         }
+    }
+    func getCurrentQuestion(currentQuestion:Int)->Question{
+            return questions[currentQuestion]
     }
     func initialSetup(){
         
@@ -60,8 +66,8 @@ class QuestionViewController: UIViewController {
         _ = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
         
         //setup Initial question text and button titles.
-        setButtonTitles(question: Grammar.getCurrentQuestion(currentQuestion: currentQuestionNumber))
-        setQuestionText(question:Grammar.getCurrentQuestion(currentQuestion: currentQuestionNumber))
+        setButtonTitles(question: getCurrentQuestion(currentQuestion: currentQuestionNumber))
+        setQuestionText(question:getCurrentQuestion(currentQuestion: currentQuestionNumber))
         //setup Question Number
         questionNumberLabel.text = "1"
     }
@@ -85,18 +91,26 @@ class QuestionViewController: UIViewController {
     }
     
     @IBAction func answerButtonAction(_ sender: UIButton) {
-        checkAnswer(question: Grammar.getCurrentQuestion(currentQuestion: currentQuestionNumber),sender: sender)
+        checkAnswer(question: getCurrentQuestion(currentQuestion: currentQuestionNumber),sender: sender)
         setupNextQuestion(questionNumber: setRandomQuestion())
         
     }
     func setRandomQuestion()->Int{
+        
+        if currentQuestionNumber < questions.count-1{
+            currentQuestionNumber = currentQuestionNumber+1
+        }else{
+            //load alert or another viewcontroller to exit and reset.
+            return 0
+        }
+        
           //create a random int for question whose isAnswered is false and set to currentQuestionNumber
-        return 1
+        return currentQuestionNumber
     }
     
     func setupNextQuestion(questionNumber:Int){
         currentQuestionNumber = questionNumber
-        currentQuestion = Grammar.getCurrentQuestion(currentQuestion: questionNumber)
+        currentQuestion = getCurrentQuestion(currentQuestion: questionNumber)
         setButtonTitles(question: currentQuestion!)
         setQuestionText(question: currentQuestion!)
         questionNumberLabel.text = "\(currentQuestionNumber+1)"
